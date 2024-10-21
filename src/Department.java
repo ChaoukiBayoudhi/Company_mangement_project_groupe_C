c57dff29a559a79e7cc0505737c65e52195f8b21
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2024-10-21 15:20:37 +0100

    second commit

--- src/Department.java
+++ src/Department.java
@@ -2,14 +2,18 @@ import lombok.*;
 
 import java.util.ArrayList;
 //@ToString //redefine the toString() method by returning the value of all fields
-@ToString(exclude = {"description","employees"})
+//@ToString(exclude = {"description","employees"})
+//@ToString(onlyExplicitlyIncluded = true)
+@ToString(of = {})
 @Getter
 @Setter
 @RequiredArgsConstructor //a constructor with only required fields as parameters
 @AllArgsConstructor
 public class Department {
+    @ToString.Include
     @NonNull
     private int id;
+    @ToString.Include
     @NonNull //the name is required
     private String name;
     private String description;
