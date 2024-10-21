94ae9352e59915eb42837b504284fde4bcd0a746
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2024-10-21 16:13:18 +0100

    third commit

--- src/Department.java
+++ src/Department.java
@@ -4,16 +4,21 @@ import java.util.ArrayList;
 //@ToString //redefine the toString() method by returning the value of all fields
 //@ToString(exclude = {"description","employees"})
 //@ToString(onlyExplicitlyIncluded = true)
-@ToString(of = {})
+@ToString(of = {"id","name","description"})
 @Getter
 @Setter
 @RequiredArgsConstructor //a constructor with only required fields as parameters
 @AllArgsConstructor
+//@EqualsAndHashCode //redefine the equals method and the hashCode method
+//compare two departments using all fields
+//@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(exclude = "employees")//compare using all fields except employees
 public class Department {
-    @ToString.Include
+   // @ToString.Include
+    //@EqualsAndHashCode.Include
     @NonNull
     private int id;
-    @ToString.Include
+   // @ToString.Include
     @NonNull //the name is required
     private String name;
     private String description;
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,19 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Department d1=new Department(1,"HR");//call the required args constructor
+        Department d11=new Department(1,"Computer Science");
         Department d2=new Department(2,"Sales","Sales Department",new ArrayList<>()); // call the all args constructor
+        Department d3=new Department(3,"Sales","Sales Department",new ArrayList<>()); // call the all args constructor
         System.out.println(d1);
         System.out.println(d2);
+        System.out.println(d3);
+        if(d3.equals(d2))
+            System.out.println("The same department");
+        else
+            System.out.println("Different departments");
+    if(d1.equals(d11))
+            System.out.println("The same department");
+        else
+            System.out.println("Different departments");
     }
 }
